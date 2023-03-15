c7e61aed00cf1de1ef7ef4ae0cd9b6af549bce00
misspia <pia_leung@hotmail.com>
2023-03-15 00:32:00 +0000

    import sketch

---
+++ src/audio/meta.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://raw.githubusercontent.com/misspia/assets/master/audio/';
+
+export const audioMeta = {
+  url: `${BASE_URL}blue-d-nobody.mp3`,
+  title: `Nobody (ft. MINO) - Blue.D`,
+}
